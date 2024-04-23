d6f6d727704306c32a0ba3ada31f1704f3a4dc2b
JGLoot <146401997+JGLoot@users.noreply.github.com>
2024-04-23 16:25:07 -0300

    Update jsprodutos.js

--- jsprodutos.js
+++ jsprodutos.js
@@ -1,13 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    function atualizarQuantidadeProdutosSalvos() {
         var produtosSalvos = JSON.parse(localStorage.getItem('carrinho')) || [];
         var quantidadeProdutosSalvos = produtosSalvos.length;
         var quantidadeProdutosSalvosElemento = document.querySelector('.quanticar');
     	quantidadeProdutosSalvosElemento.textContent = quantidadeProdutosSalvos;
-    }
-
-    // Chamar a função inicialmente
-    atualizarQuantidadeProdutosSalvos();
     executarPesquisa();
 });
 
